1a123923a535c3230d4c6b672365e2036ea013d7
SrAtom210 <mratom210@gmail.com>
2025-08-26 13:35:16 -0600

    Se agrego comentario en el main

--- app/src/main/java/com/example/primerdia/MainActivity.kt
+++ app/src/main/java/com/example/primerdia/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrimerDiaTheme {
+                //Felipe de Jesus Gonzalez Molina
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
